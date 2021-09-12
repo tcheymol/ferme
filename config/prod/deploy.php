de55494ec2538b8e2013d61dc84f1aa69a53ff26
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2021-09-12 18:35:45 +0200

    🔊 [Monolog] Add logs

--- config/prod/deploy.php
+++ config/prod/deploy.php
@@ -22,6 +22,8 @@ return new class extends DefaultDeployer {
                 'vendor/',
                 'node_modules/',
                 'public/bundles/',
+                'public/build/',
+                'public/images/',
             ]);
     }
 
