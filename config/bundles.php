265a98e6c5f025f6824341997e8d2232ae5a1431
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2021-09-12 14:36:06 +0200

    ✨ [Turbo] Enable

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyDeployBundle\EasyDeployBundle::class => ['dev' => true, 'test' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
 ];
